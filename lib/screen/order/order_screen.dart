a26046c442ec9ffb7486dbbcc48c84a8c6309281
nnsang1309 <nguyennhatsang2002@gmail.com>
2024-09-16 10:49:27 +0700

    your orders

--- lib/screen/order/order_screen.dart
+++ lib/screen/order/order_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:petshop/screen/order/order_item_card.dart';
+import 'package:petshop/screen/order/order_manager.dart';
+import 'package:provider/provider.dart';
 
 class OrdersScreen extends StatelessWidget {
   static const routeName = '/order';
@@ -6,8 +9,17 @@ class OrdersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ordersManager = OrdersManager();
     return Scaffold(
-      body: Text('Oders Screen'),
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      body: Consumer<OrdersManager>(builder: (ctx, ordersManager, child) {
+        return ListView.builder(
+          itemCount: ordersManager.orderCount,
+          itemBuilder: (context, i) => OrderItemCard(ordersManager.orders[i]),
+        );
+      }),
     );
   }
 }
